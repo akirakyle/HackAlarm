457761c815cbf2c7dc80ad53e964854d6d0b496b
Gawon Lee <gawonl@andrew.cmu.edu>
2014-09-13 02:52:05 -0400

    Here are my latest updates on activity_my.xml file.

--- app/src/main/java/com/klee2011/hackalarm/MyActivity.java
+++ app/src/main/java/com/klee2011/hackalarm/MyActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MyActivity extends Activity {
