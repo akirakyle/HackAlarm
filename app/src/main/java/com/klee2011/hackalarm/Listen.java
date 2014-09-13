b51bf2cdf16fa9db65653d44a6c23d6481bc3e19
akyle9 <akyle@andrew.cmu.edu>
2014-09-13 10:39:23 -0400

    The last commit I made was a little weird

--- app/src/main/java/com/klee2011/hackalarm/Listen.java
+++ app/src/main/java/com/klee2011/hackalarm/Listen.java
@@ -7,7 +7,6 @@ import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.view.Menu;
 import android.view.MenuItem;
-<<<<<<< HEAD
 import android.speech.RecognitionListener;
 import android.view.View;
 import android.widget.Button;
@@ -26,25 +25,15 @@ public class Listen extends Activity {
         listen_button = (Button) findViewById(R.id.button);
 
         //Listening to button event
-        listen_button.setOnClickListener(new View.OnClickListener() {
+        listen_button.setOnClickListener(R.OnClickListener() {
             onClick();
         });
     }
 
-    @Override
-    public void onClick() {
-        SpeechRecognizer sr = SpeechRecognizer.createSpeechRecognizer(getApplicationContext());
-=======
-import android.view.View;
-import android.speech.RecognitionListener;
-
-
-public class Listen extends Activity {
-    private SpeechRecognizer sr;
 
     public void onClick(View view) {
+        SpeechRecognizer sr = SpeechRecognizer.createSpeechRecognizer(getApplicationContext());
         sr = SpeechRecognizer.createSpeechRecognizer(getApplicationContext());
->>>>>>> FETCH_HEAD
         MyRecognitionListener listener = new MyRecognitionListener();
         sr.setRecognitionListener((RecognitionListener) listener);
         sr.startListening(RecognizerIntent.getVoiceDetailsIntent(getApplicationContext()));
