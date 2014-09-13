9a74b816c5f7d924e306cc1605d02dd8ce0171d2
akyle9 <akyle@andrew.cmu.edu>
2014-09-13 16:24:40 -0400

    ...

---
+++ app/src/main/java/com/klee2011/hackalarm/Listen.java
@@ -0,0 +1,138 @@
+package com.klee2011.hackalarm;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Listen extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speech_recognizer);
+        Button listen = (Button) findViewById(R.id.listen);
+        final TextView listenStat = (TextView) findViewById(R.id.listenStat);
+        final TextView listenResults = (TextView) findViewById(R.id.listenResults);
+
+
+        class MyRecognitionListener implements RecognitionListener {
+
+            @Override
+            public void onBeginningOfSpeech() {
+                listenStat.setText("onBeginningOfSpeech");
+                Log.d("Speech", "onBeginningOfSpeech");
+            }
+
+            @Override
+            public void onBufferReceived(byte[] buffer) {
+                listenStat.setText("onBufferReceived");
+                Log.d("Speech", "onBufferReceived");
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+                listenStat.setText("onEndOfSpeech");
+                Log.d("Speech", "onEndOfSpeech");
+            }
+
+            @Override
+            public void onError(int error) {
+                String str = "onError: " + Integer.toString(error);
+                Log.d("Speech", str);
+                listenStat.setText(str);
+            }
+
+            @Override
+            public void onEvent(int eventType, Bundle params) {
+                listenStat.setText("onEvent");
+                Log.d("Speech", "onEvent");
+            }
+
+            @Override
+            public void onPartialResults(Bundle partialResults) {
+                listenStat.setText("onPartialResults");
+                Log.d("Speech", "onPartialResults");
+            }
+
+            @Override
+            public void onReadyForSpeech(Bundle params) {
+                listenStat.setText("onReadyForSpeech");
+                Log.d("Speech", "onReadyForSpeech");
+            }
+
+
+            @Override
+            public void onResults(Bundle results) {
+                listenStat.setText("onResults");
+                Log.d("Speech", "onResults");
+                ArrayList<String> strResults = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                for (int i = 0; i < strResults.size();i++ ) {
+                    //Log.d("Speech", "result=" + strResults.get(i));
+                    listenResults.setText(strResults.get(i));
+                }
+                if (listenResults.getText() == "the quick brown fox jumped over the lazy dog.") {
+                    listenStat.append(" - sucess!!!");
+                    Thread.sleep(1000);
+                    Intent nextScreen = new Intent(this, MyActivity.class);
+                    startActivity(nextScreen);
+                }
+                else {
+                    listenStat.append(" - try again :(");
+                }
+            }
+
+            @Override
+            public void onRmsChanged(float rmsdB) {
+                //listenStat.setText("onRmsChanged");
+                //Log.d("Speech", "onRmsChanged");
+            }
+
+        }
+        //Listening to button event
+        listen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SpeechRecognizer sr = SpeechRecognizer.createSpeechRecognizer(getApplicationContext());
+                MyRecognitionListener listener = new MyRecognitionListener();
+                sr.setRecognitionListener(listener);
+                sr.startListening(RecognizerIntent.getVoiceDetailsIntent(getApplicationContext()));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.speech_recognizer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
