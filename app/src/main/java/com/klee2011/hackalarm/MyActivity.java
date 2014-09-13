29e6736b9c37079dae46ecde1b8294a0a9a6e93e
akyle9 <akyle@andrew.cmu.edu>
2014-09-13 18:30:37 -0400

    ...

--- app/src/main/java/com/klee2011/hackalarm/MyActivity.java
+++ app/src/main/java/com/klee2011/hackalarm/MyActivity.java
@@ -53,26 +53,7 @@ public class MyActivity extends Activity {
 
             setup();
 */
-<<<<<<< HEAD
-        Button listenAct = (Button) findViewById(R.id.listenAct);
 
-        listenAct.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goListen();
-            }
-        });
-=======
-<<<<<<< HEAD
-=======
-        
->>>>>>> FETCH_HEAD
-
-    }
-
-    public void goListen(){
-        Intent nextScreen = new Intent(this, Listen.class);
-        startActivity(nextScreen);
     }
 
     public void add(View view){
