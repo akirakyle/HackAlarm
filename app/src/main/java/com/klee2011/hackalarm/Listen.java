9053e4d1b88952aa7c26daa69cd71932dc555b5e
akyle9 <akyle@andrew.cmu.edu>
2014-09-13 10:24:55 -0400

    updates to the listen class and interface

--- app/src/main/java/com/klee2011/hackalarm/Listen.java
+++ app/src/main/java/com/klee2011/hackalarm/Listen.java
@@ -1,19 +1,38 @@
 package com.klee2011.hackalarm;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.speech.RecognitionListener
+import android.speech.RecognitionListener;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
 
 
 public class Listen extends Activity {
-    private SpeechRecognizer sr;
+
+    Button listen_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.listen);
+
+        listen_button = (Button) findViewById(R.id.button);
+
+        //Listening to button event
+        listen_button.setOnClickListener(new View.OnClickListener() {
+            onClick();
+        });
+    }
+
     @Override
     public void onClick() {
-        sr = SpeechRecognizer.createSpeechRecognizer(getApplicationContext());
+        SpeechRecognizer sr = SpeechRecognizer.createSpeechRecognizer(getApplicationContext());
         MyRecognitionListener listener = new MyRecognitionListener();
         sr.setRecognitionListener(listener);
         sr.startListening(RecognizerIntent.getVoiceDetailsIntent(getApplicationContext()));
